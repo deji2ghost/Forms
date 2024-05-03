07e5649c28bbfe556f815dbeb11a454e24f72773
deji2ghost <dejiwilliams9@gmail.com>
2024-05-03 10:58:47 +0200

    new commit

--- src/components/PortfolioPage.tsx
+++ src/components/PortfolioPage.tsx
@@ -1,6 +1,5 @@
 import { faHand } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
 
 export const PortfolioPage = () => {
   return (
